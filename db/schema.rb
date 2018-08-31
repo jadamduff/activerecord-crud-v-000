ebbc3fde959b9fad1a182311fccc571a1c593565
Adam Duff <j.adam.duff@gmail.com>
2018-08-31 06:19:10 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_31_061458) do
+ActiveRecord::Schema.define(version: 2018_08_31_061833) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -18,8 +18,6 @@ ActiveRecord::Schema.define(version: 2018_08_31_061458) do
     t.string "director"
     t.string "lead"
     t.boolean "in_theaters"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
