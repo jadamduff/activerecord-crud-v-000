83c1211fd9acfc5c8afa1fb7156593a6726304da
Adam Duff <j.adam.duff@gmail.com>
2018-08-31 05:59:37 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,7 +24,7 @@ def can_be_created_with_a_hash_of_attributes
   movie.save
 end
 
-def can_be_created_in_a_block(args = {director: "George Roy Hill", lead: "Paul Newman", in_theaters: false})
+def can_be_created_in_a_block(args = {title: "The Room", director: "George Roy Hill", lead: "Paul Newman", in_theaters: false})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
