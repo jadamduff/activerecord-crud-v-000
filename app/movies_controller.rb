eb1b417b8177cf79f1d47e1185a8f4d539f41912
Adam Duff <j.adam.duff@gmail.com>
2018-08-31 06:26:12 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,6 +22,7 @@ def can_be_created_with_a_hash_of_attributes
   }
   movie = Movie.new(attributes)
   movie.save
+  movie
 end
 
 def can_be_created_in_a_block(args = {})
@@ -39,19 +40,19 @@ def can_be_created_in_a_block(args = {})
 end
 
 def can_get_the_first_item_in_the_database
-  Movies.first
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.find(1)
 end
 
 def can_find_by_multiple_attributes
@@ -59,7 +60,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  Movie.find_by(:title => "Title", :release_date => 2000, :director => "Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
